17c1937a3515cff7ced8a1eb4483ce8edcd8128e
ycavalheiro <ygorcavalheiro93@gmail.com>
2026-06-30 10:48:16 -0300

    UPDATE: Documentação

--- app/models/professor.py
+++ app/models/professor.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from app.models.base import Base  # Certifique-se de que sua classe Base está em base.py
+from app.models.base import Base 
 
 class Professor(Base):
     """Representa um professor/personal trainer autenticado no sistema.
